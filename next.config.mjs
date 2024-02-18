aa0f69654eef00877fc3288d43b3389ce4e48527
Alistair <alistairgarioch@gmail.com>
2024-02-18 18:05:10 +0000

    feat(next.config): add redirects for /videos and /gigs to relevant page sections

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/gigs",
+        destination: "/#gigs",
+        permanent: true,
+      },
+      {
+        source: "/videos",
+        destination: "/#videos",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
