36ab58c2b1da8e646006c1aa3ddc92b6854faa6a
Alistair <alistairgarioch@gmail.com>
2024-02-18 17:20:50 +0000

    feat(metadata): add next-seo component and populate home page metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="icon" href="/favicon.svg" />
       <link rel="stylesheet" href="https://use.typekit.net/ork1mfl.css"></link>
       <Head />
       <body className="font-body antialiased">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
+import { NextSeo } from "next-seo";
 import About from "@/components/about";
 import Gigs from "@/components/gigs";
 import Hero from "@/components/hero";
 import Newsletter from "@/components/newsletter";
 
+const pageDescription =
+  "Ali Woods is an award winning stand-up comedian, podcaster, content-creator and ginger. View upcoming gigs, videos and exclusive content.";
+
+const HomeSeo = () => (
+  <NextSeo
+    title="Ali Woods | Comedian"
+    description={pageDescription}
+    openGraph={{
+      title: "Ali Woods | Comedian",
+      type: "website",
+      description: pageDescription,
+      images: [
+        {
+          url: "https://www.aliwoodsgigs.com/images/ali-woods-splits.png",
+          width: 4096,
+          height: 1224,
+          alt: "Ali Woods",
+        },
+      ],
+    }}
+  />
+);
+
 export default function Home() {
   return (
     <>
+      <HomeSeo />
       <Hero />
       <Gigs />
       <Newsletter />
