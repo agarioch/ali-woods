7cb0b4d039881c640cfdc30a8736661a0cd176ad
Alistair <alistairgarioch@gmail.com>
2024-02-25 14:16:23 +0000

    feat(analytics): add google analytics script

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import Layout from "@/components/layout";
-import "@/styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID ?? ""} />
     </Layout>
   );
 }
