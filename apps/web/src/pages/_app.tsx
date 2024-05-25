012383d237fed26af7ee59f68daa60361eea337d
Alistair <alistairgarioch@gmail.com>
2024-05-25 14:43:54 +0100

    feat(styles): update styles to use new color scheme and fonts

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { Rubik } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import Layout from "@/components/layout";
 
-const rubik = Rubik({
-  weight: ["400", "500", "700", "800"],
-  style: ["normal", "italic"],
+const space = Space_Grotesk({
+  weight: ["400", "500", "700"],
+  style: ["normal"],
   subsets: ["latin"],
-  variable: "--font-rubik",
+  variable: "--font-space-grotesk",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout className={`${rubik.className} ${rubik.variable}`}>
+    <Layout className={`${space.className} ${space.variable}`}>
       <Component {...pageProps} />
       <GoogleAnalytics
         gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? ""}
