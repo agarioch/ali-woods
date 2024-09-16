67eeb8f89aa0d020d80b0af7b90073a915072abe
Alistair <alistairgarioch@gmail.com>
2024-09-16 19:52:59 +0100

    feat: update hero colors to match new design

--- apps/web/src/components/hero/hero.tsx
+++ apps/web/src/components/hero/hero.tsx
@@ -5,15 +5,15 @@ const Hero = () => {
   return (
     <div className="container mt-52">
       <div className="relative">
-        <section className="relative z-10 flex flex-col items-center justify-between gap-10 border-4 border-black bg-white px-10 py-10 shadow-md sm:flex-row sm:px-20">
+        <section className="relative z-10 flex flex-col items-center justify-between gap-10 border-4 border-black bg-green px-10 py-10 shadow-md sm:flex-row sm:px-20">
           <div className="max-w-max font-heading md:block">
-            <p className="text-3xl font-bold lg:text-4xl  xl:text-5xl">
-              In a world of <span className="text-green">wannabes</span>,
+            <p className="text-3xl font-bold text-white lg:text-4xl xl:text-5xl">
+              In a world of wannabes,
               <br />
-              <span className="text-blue">Woods</span>
+              Woods
               {` is the real deal"`}
             </p>
-            <p className="mt-4 font-medium text-gray lg:text-xl">
+            <p className="mt-4 font-medium text-yellow lg:text-xl">
               Evening Standard
             </p>
           </div>
