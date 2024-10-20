4c3f75037c4a18f50ce6d1b8de9cf78d96e9e900
Alistair <alistairgarioch@gmail.com>
2024-10-20 17:00:05 +0100

    chore(hero): add missing comma before quote

--- apps/web/src/components/hero/hero.tsx
+++ apps/web/src/components/hero/hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <section className="relative z-10 flex flex-col items-center justify-between gap-10 border-4 border-black bg-green px-10 py-10 shadow-md sm:flex-row sm:px-20">
           <div className="max-w-max font-heading md:block">
             <p className="text-3xl font-bold text-white lg:text-4xl xl:text-5xl">
-              In a world of wannabes,
+              {`"In a world of wannabes,`}
               <br />
               Woods
               {` is the real deal"`}
